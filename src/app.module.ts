a4bce8799c983ef51ddae6712a01c0f7d0e0f5ff
blAs1N <qazasa123@gmail.com>
2025-11-30 03:48:20 +0900

    Create base rest api

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [PrismaModule],
+  imports: [PrismaModule, ProductModule],
 })
 export class AppModule {}
