5a7ff5425ec3f6b7b0216b021bd1b061818461df
blAs1N <qazasa123@gmail.com>
2025-11-30 19:20:05 +0900

    Setup graphql resolver

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductModule } from './product/product.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [PrismaModule, ProductModule],
+  imports: [PrismaModule, ProductModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+      debug: true,
+      playground: true
+    })
+  ],
 })
 export class AppModule {}
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { ProductResolver } from './product.resolver';
 
 @Module({
   controllers: [ProductController],
-  providers: [ProductService],
+  providers: [ProductService, ProductResolver],
   imports: [PrismaModule]
 })
 export class ProductModule {}
